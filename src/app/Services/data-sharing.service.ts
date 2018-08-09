0f998e59c77d8ea60f7a3a5bad4aa6c897f8871a
Donny Cheng <donny.cheng@slalom.com>
2018-08-09 16:41:26 -0400

    changed BehaviorSubject to Subject

--- src/app/Services/data-sharing.service.ts
+++ src/app/Services/data-sharing.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Subject } from 'rxjs';
 
 @Injectable()
 export class DataSharingService {
 
-    private numberSource = new BehaviorSubject<number>(0);
-    public numberSourceObservable = this.numberSource.asObservable();
+    // private numberSource = new BehaviorSubject<number>(0);
+    // public numberSourceObservable = this.numberSource.asObservable();
+    public numberSource = new Subject<number>();
     constructor() { }
 
     numberSelected(num: number){
