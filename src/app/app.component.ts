51654ee9feee4fc835576059963513ce79aac9ff
Donny Cheng <donccl@gmail.com>
2018-08-11 14:54:43 -0400

    matching function is done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,7 +26,25 @@ export class AppComponent implements OnInit, OnDestroy {
         console.log("check number now: " + this.userSelectedDisplay.numberArray);
         console.log("check number now: " + this.loteryDisplay.numberArray);
 
+        let res: Set<number> = this.checkMatchingNumber(this.loteryDisplay.numberArray, this.userSelectedDisplay.numberArray)
+        res.forEach((val: number) => {
+            console.log(`value: ${val}`);
+        });
         
+    }
+
+    private checkMatchingNumber(lotteryNumbers: Array<number>, userNumbers: Array<number>): Set<number>{
+        let lotterySet = new Set(lotteryNumbers);
+        let userNumberSet = new Set(userNumbers);
+        let machingSet = new Set();
+
+        lotterySet.forEach((num) => {
+            if (userNumberSet.has(num))
+                machingSet.add(num);
+        });
+
+        return machingSet;
+
     }
 
     ngOnDestroy(){
